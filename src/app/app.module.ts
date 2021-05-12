d3842c373c4d5d04dc3ed0ee29d3da59d2bd83bc
Blerina Dako <bdako@helius-software.com>
2021-05-12 09:58:22 +0200

    login files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+
+import { JwtInterceptor, ErrorInterceptor, AuthGuard } from './helpers';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    NgbModule,FormsModule,HttpClientModule
+    NgbModule,FormsModule,HttpClientModule,ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AuthGuard,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/helpers/auth.guard.ts
@@ -0,0 +1,24 @@
+﻿import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+
+import { AuthenticationService } from "../../app/services/user-service.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) { }
+
+    canActivate():boolean {
+        const currentUser = this.authenticationService.currentUserValue;
+        if (currentUser) {
+            // logged in so return true
+            return true;
+        }
+
+        // not logged in so redirect to login page with the return url
+        this.router.navigate(['/login']);
+        return false;
+    }
+}
